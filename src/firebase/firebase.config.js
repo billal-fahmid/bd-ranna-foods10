f4f6f595ca746479dae28e281384308561eb0968
billal-fahmid <billalcooom@gmail.com>
2023-05-03 10:04:19 +0600

    hide firebase config by environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDKjzwPGCbOmwoANc6MsmHPIuxJGMplLUU",
-  authDomain: "chef-hunter-assignment-10.firebaseapp.com",
-  projectId: "chef-hunter-assignment-10",
-  storageBucket: "chef-hunter-assignment-10.appspot.com",
-  messagingSenderId: "352595169619",
-  appId: "1:352595169619:web:4d9964a0b4819c9be211a6"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
